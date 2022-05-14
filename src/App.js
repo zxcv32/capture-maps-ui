cf43f7c8ff4cd408e01549ccdfccc0bb65b48381
Ashwani Sharma <i14a23h19a@gmail.com>
2022-05-14 11:50:23 +0530

    wip: wedding

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
 import './App.css';
-import React from 'react'
-import {MapAp, specsx} from "./component/Map"
+import {React, useState} from 'react'
+import {MapAp, specs} from "./component/Map"
+import configData from './config'
 
 function App() {
 
+  const defaultZoom = 15
+  const defaultRadius = 5
+  const [zoom, setZoom] = useState(defaultZoom);
+  const [radius, setRadius] = useState(defaultRadius);
+
   function Send(props) {
-    console.log(
-        "lat: " + props.lat + " lng: " + props.lng + " zoom: " + props.zoom);
+    props.zoom = zoom
+    props.radius = radius
+    console.log(JSON.stringify(props))
+    const requestOptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      mode: 'no-cors',
+      body: JSON.stringify(props),
+      responseType: "blob"
+    };
+    const url = configData.API_HOST + "print"
+    fetch(url, requestOptions)
+    .then(response => {
+      response.arrayBuffer().then(function (buffer) {
+        const url = window.URL.createObjectURL(new Blob([buffer]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "capture.png");
+        document.body.appendChild(link);
+        // link.click();
+      });
+    })
+
+    .catch(err => {
+      console.log(err);
+    });
   }
 
   return (
       <div className="App">
-        <div><p>Capture Maps</p>
-          <button onClick={() => Send(specsx)}>Capture</button>
+        <div><h1>Capture Maps</h1>
+          <InputForm/>
         </div>
         <MapAp/>
       </div>
   );
+
+  function InputForm() {
+    return (
+        <form>
+          <label>Zoom
+            <input type="number" placeholder={defaultZoom}
+                   value={zoom}
+                   onChange={(e) => {
+                     setZoom(parseInt(e.target.value))
+                   }}/>
+          </label>
+          <br/>
+          <label>Radius
+            <input type="number" placeholder={defaultRadius}
+                // defaultValue={defaultRadius}
+                   value={radius}
+                   onChange={(e) => {
+                     setRadius(parseInt(e.target.value))
+                   }}/>
+          </label>
+          <br/>
+          <input type="button" value={"Capture"} onClick={() => Send(specs)}/>
+        </form>
+    )
+  }
 }
 
 export default App;
--- src/component/Map.js
+++ src/component/Map.js
@@ -1,13 +1,18 @@
 import {GoogleMap, useLoadScript} from "@react-google-maps/api";
 import React from "react";
 
-const center = {lat: 30.320976, lng: 78.084581}
+const center = {lat: 30.31696355790659, lng: 78.0325606876011}
 const containerStyle = {
   width: '100%',
   height: '100%'
 };
 
-export let specsx = {lat: 0, lng: 0, zoom: 15};
+export let specs = {
+  lat: center.lat,
+  lng: center.lng,
+  zoom: 15,
+  radius: 5
+};
 
 export function MapAp() {
   const {isLoaded} = useLoadScript({
@@ -27,15 +32,17 @@ function Map() {
   const handleCenterChanged = () => {
     if (mapref) {
       const newCenter = mapref.getCenter();
-      specsx.lat = newCenter.lat();
-      specsx.lng = newCenter.lng();
-      specsx.zoom = mapref.zoom;
-      console.debug("specsx: "+specsx);
+      specs.lat = newCenter.lat();
+      specs.lng = newCenter.lng();
+      // Override from UI input
+      // specs.zoom = mapref.zoom;
+      // specs.radius = 5
+      console.debug("specs: " + specs);
     }
   };
 
   return (
-      <GoogleMap zoom={15} center={center}
+      <GoogleMap zoom={13} center={center}
                  mapContainerStyle={containerStyle} mapTypeId="hybrid"
                  onLoad={handleOnLoad}
                  onCenterChanged={handleCenterChanged}></GoogleMap>
