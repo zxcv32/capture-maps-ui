3747c3f497d913cfe83a4a86df3dbe1312d88ded
Ashwani Sharma <i14a23h19a@gmail.com>
2022-08-10 18:29:21 +0530

    test: fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders root path', () => {
+  render(<App/>);
+  const linkElement = screen.getByText('Capture Maps');
   expect(linkElement).toBeInTheDocument();
 });
