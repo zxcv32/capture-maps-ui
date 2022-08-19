35fe6167e48d7fa01098038a6e6fa722e5d03b44
Ashwani Sharma <ashwani@zxcv32.com>
2022-08-19 19:51:39 +0530

    build: update Dockerfile script

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-echo "REACT_APP_GOOGLE_MAPS_API_KEY=$REACT_APP_GOOGLE_MAPS_API_KEY" >> /usr/share/nginx/html/.env
-echo "REACT_APP_API_HOST=$REACT_APP_API_HOST" >> /usr/share/nginx/html/.env
+echo "REACT_APP_GOOGLE_MAPS_API_KEY='$REACT_APP_GOOGLE_MAPS_API_KEY'" >> /usr/share/nginx/html/.env
+echo "REACT_APP_API_HOST='$REACT_APP_API_HOST'" >> /usr/share/nginx/html/.env
